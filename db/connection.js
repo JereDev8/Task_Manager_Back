f44d1574dd822124fab143457dcf59bf7b6e8d44
Jeremias Quinteros <quinterosjeremias8@gmail.com>
2024-12-30 14:15:37 -0300

    db cloud

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/tasks');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('Conexión a MongoDB exitosa');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error);
